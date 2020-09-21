042bcdc9342c98cdcb58bb75540fee5a3e891268
cmanthou <cory.manthou@gmail.com>
2020-09-21 14:13:04 -0400

    add server and models

---
+++ models/workout.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Please enter the exercise type"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "What is the exercise name"
+            },
+            duration: {
+                type: Number,
+                required: "Please enter the duration of the exercise"
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            }
+        }
+    ]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
