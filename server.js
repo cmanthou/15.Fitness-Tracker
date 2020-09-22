04f1ecca9020c15ba69316afb5ab768e2420bde1
cmanthou <cory.manthou@gmail.com>
2020-09-22 14:03:13 -0400

    Removed Commented Sections

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
-// const compression = require("compression");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// app.use(compression());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,8 +11,6 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessdb", {
   useNewUrlParser: true,
-  // useUnifiedTopology: true,
-  // useCreateIndex: true,
   useFindAndModify: false,
 });
 
